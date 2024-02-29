ea5660df78e0c8984f88886fa6d84642f92c7b97
Kano.M <itspapari888@gmail.com>
2024-02-29 22:46:58 +0900

    ファイル内容の更新

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Todocontroller;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/',[Todocontroller::class, 'index']);
+Route::post('/todos', [Todocontroller::class, 'store']);
+Route::patch('/todos/update',[Todocontroller::class, 'update']);
+Route::delete('/todos/delete', [Todocontroller::class, 'destroy']);
+Route::get('/todos/search',[Todocontroller::class,'search']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/categories',[CategoryController::class,'store']);
+Route::patch('/categories/update',[CategoryController::class,'update']);
+Route::delete('/categories/delete',[CategoryController::class, 'destroy']);
